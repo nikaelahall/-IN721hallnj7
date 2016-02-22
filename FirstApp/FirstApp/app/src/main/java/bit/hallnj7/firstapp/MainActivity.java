7e0ad3d823f755eaf71588fc4fff72086695303c
nikaelahall <hallnj7@student.op.ac.nz>
2016-02-22 17:22:02 +1300

    Completed in class task

--- FirstApp/FirstApp/app/src/main/java/bit/hallnj7/firstapp/MainActivity.java
+++ FirstApp/FirstApp/app/src/main/java/bit/hallnj7/firstapp/MainActivity.java
@@ -2,6 +2,9 @@ package bit.hallnj7.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtRandomString = (TextView)findViewById(R.id.authorText);
+
+        //pick a string
+        String dogBreed = "";
+
+        Random rGen = new Random();
+        int dogValue = rGen.nextInt(4);
+
+        switch(dogValue) {
+            case 0:
+                dogBreed = "Poodle";
+                break;
+            case 1:
+                dogBreed = "Labrador";
+                break;
+            case 2:
+                dogBreed = "Shar Pei";
+                break;
+            case 3:
+                dogBreed = "Newfoundland";
+                break;
+        }
+
+        txtRandomString.setText(dogBreed);
     }
 }
