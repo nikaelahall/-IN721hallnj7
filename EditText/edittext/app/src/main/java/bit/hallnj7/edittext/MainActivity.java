313361aec99b9d2dd6502e253510e96dc833d574
nikaelahall <hallnj7@student.op.ac.nz>
2016-02-25 19:59:29 +1300

    Completed the @ click and the username entering into the EditText

--- EditText/edittext/app/src/main/java/bit/hallnj7/edittext/MainActivity.java
+++ EditText/edittext/app/src/main/java/bit/hallnj7/edittext/MainActivity.java
@@ -25,10 +25,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onKey(View v, int keyCode, KeyEvent event)
         {
-            v.getId();
+            int viewID = v.getId();
+            EditText etInput = (EditText)findViewById(viewID);
+            String userName = etInput.getText().toString();
+
             if (keyCode == KeyEvent.KEYCODE_AT)
             {
                 Toast.makeText(MainActivity.this, "That was at...", Toast.LENGTH_LONG).show();
+            }
+
+
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                if (userName.length() < 8) {
+                    Toast.makeText(MainActivity.this, "Must be 8 characters, " + userName, Toast.LENGTH_LONG).show();
+                }
+
+                else {
+                    Toast.makeText(MainActivity.this, "Thank you, " + userName, Toast.LENGTH_LONG).show();
+                }
             }
             return false;
         }
