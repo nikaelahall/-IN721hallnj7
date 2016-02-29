957a27aa24117d3cf2a50bc1ccac18cf82c956a6
nikaelahall <hallnj7@student.op.ac.nz>
2016-02-29 16:32:13 +1300

    Created the array and the month spinner

--- ScreenControls/ScreenControls/app/src/main/java/bit/hallnj7/screencontrols/MainActivity.java
+++ ScreenControls/ScreenControls/app/src/main/java/bit/hallnj7/screencontrols/MainActivity.java
@@ -1,21 +1,50 @@
 package bit.hallnj7.screencontrols;
 
+import android.content.res.Resources;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources resourceResolver = getResources();
 
+        Button confirmButton = (Button) findViewById(R.id.ConfirmationButton);
         RadioGroup instrumentGroup = (RadioGroup) findViewById(R.id.rbgrpInstruments);
+
+        String[] months =
+                {
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                };
+
+        Spinner monthsSpinner = (Spinner)findViewById(R.id.monthSpinner);
+        int layoutID = android.R.layout.simple_spinner_item;
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(this, layoutID, months);
+        monthsSpinner.setAdapter(monthAdapter);
+
+        confirmButton.setOnClickListener(new radioGroupListener());
         instrumentGroup.setOnClickListener(new radioGroupListener()); //creates the new class for radioGroupListener
     } //end on create
 
@@ -24,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v)
         {
-            Toast.makeText(MainActivity.this, "You have enrolled for  .... lessons", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "You have enrolled for <instrument> lessons in <month>", Toast.LENGTH_LONG).show();
         }
     }
 }
