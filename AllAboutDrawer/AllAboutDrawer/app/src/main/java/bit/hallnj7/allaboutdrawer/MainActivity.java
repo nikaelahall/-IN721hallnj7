c357136d70bf4f9d9887247360bbe486b50b8f5b
nikaelahall <hallnj7@student.op.ac.nz>
2016-03-10 18:33:28 +1300

    Added a bit of appearance to each of the different activities, as well as some comments

--- AllAboutDrawer/AllAboutDrawer/app/src/main/java/bit/hallnj7/allaboutdrawer/MainActivity.java
+++ AllAboutDrawer/AllAboutDrawer/app/src/main/java/bit/hallnj7/allaboutdrawer/MainActivity.java
@@ -15,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
 
-        setUpDunedinGroupList();
+        setUpDunedinGroupList(); //calls the class setUpDunedinGroupList
 
-        ListView dunedinGroupListView = (ListView) findViewById(R.id.left_drawer);
+        ListView dunedinGroupListView = (ListView) findViewById(R.id.left_drawer); //finds the id of the listView
         dunedinGroupListView.setOnItemClickListener(new DunedinGroupListClickHandler());
     }
 
     public void setUpDunedinGroupList() {
-        String[] groups = {"Services", "Fun things to do", "Dining", "Shopping"};
-        ArrayAdapter<String> dunedinGroupAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groups); //FIX THIS!
+        String[] groups = {"Services", "Fun things to do", "Dining", "Shopping"}; //creates the array for the groups in Dunedin
+        ArrayAdapter<String> dunedinGroupAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groups);
         ListView dunedinGroupListView = (ListView) findViewById(R.id.left_drawer);
         dunedinGroupListView.setAdapter(dunedinGroupAdapter);
     }
@@ -31,21 +31,21 @@ public class MainActivity extends AppCompatActivity {
     public class DunedinGroupListClickHandler implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            String clickedItem = (String) parent.getItemAtPosition(position).toString();
+            String clickedItem = (String) parent.getItemAtPosition(position).toString(); //gets the position of the clicked item
             Intent goToIntent;
 
-            switch (clickedItem) {
+            switch (clickedItem) { //creates the switch for each of the list items
                 case "Services":
-                    goToIntent = new Intent(MainActivity.this, ServiceActivity.class);
+                    goToIntent = new Intent(MainActivity.this, ServiceActivity.class); //goes to the service class
                     break;
-                case "Fun things to do":
-                    goToIntent = new Intent(MainActivity.this, FunThingsToDoActivity.class);
+                case "Fun things to do": //if the activities list item is selected
+                    goToIntent = new Intent(MainActivity.this, FunThingsToDoActivity.class); //goes to the activities class
                     break;
-                case "Dining":
-                    goToIntent = new Intent(MainActivity.this, DiningActivity.class);
+                case "Dining": //if the dining list item is selected
+                    goToIntent = new Intent(MainActivity.this, DiningActivity.class); //goes to the dining class
                     break;
-                case "Shopping":
-                    goToIntent = new Intent(MainActivity.this, ShoppingActivity.class);
+                case "Shopping": //if the shopping list item is selected
+                    goToIntent = new Intent(MainActivity.this, ShoppingActivity.class); //goes to the shopping class
                     break;
                 default:
                     goToIntent = null;
