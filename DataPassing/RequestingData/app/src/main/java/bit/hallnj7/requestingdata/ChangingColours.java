9d01a02682245b342d1fa1d78c490a083a42aac6
nikaelahall <hallnj7@student.op.ac.nz>
2016-03-17 22:17:51 +1300

    Got the Color changing app working

--- DataPassing/RequestingData/app/src/main/java/bit/hallnj7/requestingdata/ChangingColours.java
+++ DataPassing/RequestingData/app/src/main/java/bit/hallnj7/requestingdata/ChangingColours.java
@@ -1,5 +1,8 @@
 package bit.hallnj7.requestingdata;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class ChangingColours extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changing_colours);
+
+        Intent changeActivityIntent = new Intent();
+
+        //not sure why this getColor is underlined, but this line of code still works?
+        String strRedColor = "#" + Integer.toHexString(getResources().getColor(R.color.red, null));
+
+        //int strRedColor = getResources().getColor(R.color.red, null);
+       // String strRed = "#"+Integer.toHexString(strRedColor);
+
+        changeActivityIntent.putExtra("requestedResult", strRedColor);
+        setResult(Activity.RESULT_OK, changeActivityIntent);
+        finish();
     }
 }
--- DataPassing/RequestingData/app/src/main/java/bit/hallnj7/requestingdata/MainActivity.java
+++ DataPassing/RequestingData/app/src/main/java/bit/hallnj7/requestingdata/MainActivity.java
@@ -1,7 +1,14 @@
 package bit.hallnj7.requestingdata;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnChangeColour = (Button) findViewById(R.id.ColourButton);
+        btnChangeColour.setOnClickListener(new ButtonChangeColourClickHandler());
+    }
+
+    public class ButtonChangeColourClickHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            Intent changeActivityIntent = new Intent(MainActivity.this, ChangingColours.class);
+            startActivityForResult(changeActivityIntent, 0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if ((requestCode == 0) && (resultCode == Activity.RESULT_OK))
+        {
+            String result = data.getStringExtra("requestedResult");
+            TextView textChanging = (TextView) findViewById(R.id.TextViewBody);
+            textChanging.setTextColor(Color.parseColor(result));
+        }
     }
 }
