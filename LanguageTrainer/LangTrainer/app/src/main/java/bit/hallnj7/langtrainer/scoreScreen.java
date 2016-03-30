158f24d9b9e70052859988dac3078d8b0e1473d6
nikaelahall <hallnj7@student.op.ac.nz>
2016-03-30 14:26:47 +1300

    Added a retry button. Made score text uneditable

--- LanguageTrainer/LangTrainer/app/src/main/java/bit/hallnj7/langtrainer/scoreScreen.java
+++ LanguageTrainer/LangTrainer/app/src/main/java/bit/hallnj7/langtrainer/scoreScreen.java
@@ -3,6 +3,8 @@ package bit.hallnj7.langtrainer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,10 +20,23 @@ public class scoreScreen extends AppCompatActivity {
         setContentView(R.layout.activity_score_screen);
 
         Intent launchIntent = getIntent();
-        TextView scoretext = (TextView)findViewById(R.id.etScore);
+        TextView scoretext = (TextView)findViewById(R.id.tvScore);
         String calculatedScore;
         calculatedScore = launchIntent.getStringExtra("totscore"); //gets the totalScore screen from the questionActivity
 
-        scoretext.setText(calculatedScore);//Displays the total score to the TextView
+        scoretext.setText(calculatedScore + " /11");//Displays the total score to the TextView
+
+        Button tryagain = (Button)findViewById(R.id.btnTryAgain);
+        tryagain.setOnClickListener(new tryagainHandler());
+    }
+
+    class tryagainHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v)
+        {
+            Intent changeActivityIntent = new Intent(scoreScreen.this, welcomeScreen.class); //changes to the questionActivity
+            startActivity(changeActivityIntent);
+        }
     }
 }
