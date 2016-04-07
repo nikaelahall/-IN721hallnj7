f6082bd210e11b7eb83df4d1d8bdbfcb2e1af499
nikaelahall <hallnj7@student.op.ac.nz>
2016-04-07 15:54:03 +1200

    Altered the languagePreference code

--- LanguagePreference/LanguagePreference/app/src/main/java/bit/hallnj7/languagepreference/MainActivity.java
+++ LanguagePreference/LanguagePreference/app/src/main/java/bit/hallnj7/languagepreference/MainActivity.java
@@ -1,6 +1,9 @@
 package bit.hallnj7.languagepreference;
 
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,21 +28,17 @@ public class MainActivity extends AppCompatActivity {
         prefs = getSharedPreferences("prefsDemo", MODE_PRIVATE);
         prefsEditor = prefs.edit();
 
-        //fetch the stored langauge preference
-        //Returns null if the provided key-value pair hasn't been set
         String languagePreference = prefs.getString("language", null);
-        if (languagePreference != null)
+        int colorPreference = prefs.getInt("colorWanted", 0);
+
+        if ((languagePreference != null) && (colorPreference != 0))
         {
             String greetingInChosenLanguage = getGreeting(languagePreference);
+            int greetingInChosenColor = getChosenColor(colorPreference);
 
             TextView tvGreeting = (TextView)findViewById(R.id.tvChosenLanguage);
             tvGreeting.setText(greetingInChosenLanguage);
-        }
-
-        String colourPreference = prefs.getString("chosenColor", null);
-        if(colourPreference != null)
-        {
-
+            tvGreeting.setTextColor(greetingInChosenColor);
         }
     }
 
@@ -47,22 +46,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             //Find out which language radio button is selected
-            RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
-            int checkedId = rg.getCheckedRadioButtonId();
+            RadioGroup LangRg = (RadioGroup) findViewById(R.id.radioGroup1);
+            int checkedId = LangRg.getCheckedRadioButtonId();
             RadioButton checkedButton = (RadioButton) findViewById(checkedId);
             String checkedLanguage = checkedButton.getText().toString();
 
+            RadioGroup ColRg = (RadioGroup)findViewById(R.id.rgColor);
+            int checkedColorId = ColRg.getCheckedRadioButtonId();
+            RadioButton checkedRadio = (RadioButton)findViewById(checkedColorId);
+            int checkedColor = checkedRadio.getId();
+
             //store new chosen language in prefs
             prefsEditor.putString("language", checkedLanguage);
+            prefsEditor.putInt("colorWanted", checkedColor);
             prefsEditor.commit();
 
             String languagePreference = prefs.getString("language", null);
-            if (languagePreference != null)
+            int colorPreference = prefs.getInt("colorWanted", 0);
+            if ((languagePreference != null) && (colorPreference != 0))
             {
                 String greetingInChosenLanguage = getGreeting(languagePreference);
+                int greetingInChosenColor = getChosenColor(colorPreference);
 
                 TextView tvGreeting = (TextView)findViewById(R.id.tvChosenLanguage);
                 tvGreeting.setText(greetingInChosenLanguage);
+                tvGreeting.setTextColor(greetingInChosenColor);
             }
         }
     }
@@ -89,17 +97,23 @@ public class MainActivity extends AppCompatActivity {
             return greeting;
         }
 
-    private String getColor(String colorChosen)
-    {
-        String ChosenColor = "";
+    private int getChosenColor(int colorWanted)
+   {
+        int chosenColor = 0;
 
-        switch (colorChosen)
+        switch (colorWanted)
         {
-            case("Blue"):
-                ChosenColor = "hi" ;
+            case 0: //
+                chosenColor = (getResources().getColor(R.color.Green, null));
                 break;
-        }
+            case 1:
+                chosenColor = (getResources().getColor(R.color.colorPrimary, null));
+               break;
+           case 2:
+                chosenColor = (getResources().getColor(R.color.Red, null));
+                break;
+       }
 
-        return ChosenColor;
+        return chosenColor;
     }
 }
