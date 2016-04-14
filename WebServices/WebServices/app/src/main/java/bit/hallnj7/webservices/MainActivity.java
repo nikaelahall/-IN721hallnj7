64f4c40acb2aad76cce01ca750485129ae748a5a
nikaelahall <hallnj7@student.op.ac.nz>
2016-04-14 17:17:03 +0800

    Got the top artists displaying in a listView

--- WebServices/WebServices/app/src/main/java/bit/hallnj7/webservices/MainActivity.java
+++ WebServices/WebServices/app/src/main/java/bit/hallnj7/webservices/MainActivity.java
@@ -1,14 +1,17 @@
 package bit.hallnj7.webservices;
 
 import android.os.AsyncTask;
-import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,76 +20,99 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    String JSONInput;
+    ArrayList<String> artistNames;
     URL URLObject;
     HttpURLConnection connection;
+    ListView artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        artistNames = new ArrayList<>();
+
         Button btnShowRaw = (Button) findViewById(R.id.btnRaw);
-        btnShowRaw.setOnClickListener(new View.OnClickListener()
+        btnShowRaw.setOnClickListener(new ButtonClickHandler());
+    }
+
+
+    public class ButtonClickHandler implements  View.OnClickListener
+    {
+        @Override
+        public void onClick(View v)
         {
-            @Override
-            public void onClick(View v)
-            {
-                AsyncAPIShowRawJSON APIThread = new AsyncAPIShowRawJSON();
-                APIThread.execute();
-            }});
-    } //end onCreate
+            AsyncAPIShowRawJSON APIThread = new AsyncAPIShowRawJSON();
+            APIThread.execute();
+        }
+    }
 
+    public void createList(String fetchedString)
+    {
+        try {
+            JSONObject artistData = new JSONObject(fetchedString);
+            JSONObject artists = artistData.getJSONObject("artists");
+            JSONArray artistArray = artists.getJSONArray("artist");
 
+            for (int i = 0; i < artistArray.length(); i++) {
+                JSONObject artist = artistArray.getJSONObject(i);
+                String name = artist.getString("name");
+                String playcount = artist.getString("playcount");
+                artistNames.add(name + " " + playcount);
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-    class AsyncAPIShowRawJSON extends AsyncTask<Void, Void, String>
+    public class AsyncAPIShowRawJSON extends AsyncTask<Void, Void, String>
     {
         @Override
         protected String doInBackground(Void... params)
         {
-            //Declare here so it's not local to the try block
             String JSONString = null;
             try
             {
-                String urlString = "http://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=Sia&api_key=58384a2141a4b9737eacb9d0989b8a8c&limit=10&format=json";
+                String urlString = "http://ws.audioscrobbler.com/2.0/?method=chart.getTopArtists&api_key=58384a2141a4b9737eacb9d0989b8a8c&limit=10&format=json";
 
-
-                //Convert URL string to URLObject
                 URL URLObject = new URL(urlString);
-                //Create HTTPURLConnection object via openConnection command of URLObject
                 HttpURLConnection connection = (HttpURLConnection) URLObject.openConnection();
-                //Send the URL
                 connection.connect();
-                //If it doesn't return 200, you don't have data...
                 int responseCode = connection.getResponseCode();
-                //Get an InputStream from the HttpURLConnection object and set up a BufferedReader
                 InputStream inputStream = connection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                //Read the input. May only be one line...
                 String responseString;
                 StringBuilder stringBuilder = new StringBuilder();
                 while ((responseString = bufferedReader.readLine()) != null)
                 {
                     stringBuilder = stringBuilder.append(responseString);
                 }
-                //Get the string from the stringBuilder. JSONString ready for parsing
+
                 JSONString = stringBuilder.toString();
-            } //end try
+            }
 
             catch(Exception e) {e.printStackTrace();}
             return JSONString;
-        } //end doInBackground
+        }
 
 
 
         protected void onPostExecute(String fetchedString)
         {
-            //Dump fetched string to textView
-            TextView tv = (TextView) findViewById(R.id.textView);
-            tv.setText(fetchedString);
+            createList(fetchedString);
+
+            artistList = (ListView) findViewById(R.id.listView);
+            ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, artistNames);
+            artistList.setAdapter(arrayAdapter);
         }
     }
 }
