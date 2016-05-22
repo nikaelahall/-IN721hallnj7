f53c2d510bbf323d6b4a5115a734be2ed4dec296
hallnj7 <hallnj7@student.op.ac.nz>
2016-05-22 15:49:44 +1200

    Started the ripple background activity

---
+++ ThirdPartyLibraries/RippleBackground/app/src/main/java/bit/hallnj7/ripplebackground/MainActivity.java
@@ -0,0 +1,28 @@
+package bit.hallnj7.ripplebackground;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final RippleBackground rippleBackground = (RippleBackground)findViewById(R.id.content);
+        ImageView imageView = (ImageView)findViewById(R.id.centerImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rippleBackground.startRippleAnimation();
+            }
+        });
+    }
+}
